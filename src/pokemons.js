690853bd32cc38a0766cce88e9b4273c6c6d5d66
Trevor Smith <trevorsmith@Trevors-MacBook-Air.local>
2024-11-01 15:52:40 -0600

    Request all pokemon from the National pokedex

--- src/pokemons.js
+++ src/pokemons.js
@@ -1,18 +1,19 @@
-const POKEAPI_URL = "https://pokeapi.co/api/v2/pokemon";
+const POKEAPI_URL = "https://pokeapi.co/api/v2";
 
 export async function getPokemon(pokemonId) {
-    const pokemon = await fetch(`${POKEAPI_URL}/${pokemonId}`);
+    const pokemon = await fetch(`${POKEAPI_URL}/pokemon/${pokemonId}`);
     const data = await pokemon.json();
     return data;
 }
 
 export async function getPokemons() {
-    const pokemons = await fetch(`${POKEAPI_URL}?limit=151`);
+    const pokemons = await fetch(`${POKEAPI_URL}/pokedex/1/?limit=151`);
     const data = await pokemons.json();
 
     // Parse the pokemons id from the url and add it to the results
-    return data.results.map((pokemon) => ({
-        ...pokemon,
-        id: pokemon.url.split("/").at(-2),
+    return data.pokemon_entries.map((pokemon) => ({
+        id: pokemon.entry_number,
+        name: pokemon.pokemon_species.name,
+        species_url: pokemon.pokemon_species.url,
     }));
 }
