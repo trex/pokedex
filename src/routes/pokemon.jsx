adcac7d63429cf595e4059abd84417ec97886065
Trevor Smith <trevorsmith@Trevors-Air.home>
2024-11-02 18:36:33 -0600

    Card styling

--- src/routes/pokemon.jsx
+++ src/routes/pokemon.jsx
@@ -12,33 +12,45 @@ export default function Pokemon() {
     const { heightUnit, weightUnit } = useOutletContext();
 
     return (
-        <div className="pokemon-card" data-type={primaryType}>
+        <div 
+            className="pokemon-card" 
+            style={{ "--pokemon-color": pokemon.color.name }} 
+            data-type={primaryType}
+        >
             <div className="card-header">
-                <div className="types">
-                    {pokemon.types.map((type) => (
-                        <img 
-                            key={type.type.name} 
-                            src={`../type-icons/${type.type.name}.png`} 
-                            className="type-badge"
-                            alt={`${type.type.name} type`} 
-                        />
-                    ))}
+                <div className="card-header-left">
+                    <img src="../stage-2.png" alt="basic" className="stage-icon" />
+                    <h1 className="pokemon-name">{pokemon.name}</h1>
                 </div>
-                <span className="pokemon-number">No. {pokemon.id}</span>
-                <h1 className="pokemon-name">{pokemon.name}</h1>
+                <div className="card-header-right">
+                    <div className="pokemon-hp">
+                        <span className="hp-label">HP</span><span className="hp-value">{pokemon.stats.find((stat) => stat.stat.name === "hp").base_stat}</span>
+                    </div>
+                    <div className="types">
+                        {pokemon.types.map((type) => (
+                            <img 
+                                key={type.type.name} 
+                                src={`../type-icons/${type.type.name}.png`} 
+                                className="type-badge"
+                                alt={`${type.type.name} type`} 
+                            />
+                        ))}
+                    </div>
+                </div>
             </div>
             
             <div className="card-body">
                 <div className="pokemon-image-container" data-type={primaryType}>
                     <img src={pokemon.sprites.other["official-artwork"].front_default} alt={pokemon.name} className="pokemon-image" />
+                    <div className="info-row">
+                        <span className="pokemon-number">No. {pokemon.id}</span>
+                        <span className="genus">{pokemon.genera.find((genus) => genus.language.name === "en").genus}</span>
+                        <span>Height: {heightUnit.conversion(pokemon.height)} {heightUnit.abreviation}</span>
+                        <span>Weight: {weightUnit.conversion(pokemon.weight)} {weightUnit.abreviation}</span> 
+                    </div>
                 </div>
                 
                 <div className="pokemon-info">
-                    <div className="info-row">
-                        <span className="label">Height:</span> {heightUnit.conversion(pokemon.height)} {heightUnit.abreviation}
-                        <span className="label">Weight:</span> {weightUnit.conversion(pokemon.weight)} {weightUnit.abreviation}
-                    </div>
-                    
                     <div className="pokemon-description">
                         {pokemon.flavor_text_entries.find((entry) => entry.language.name === "en").flavor_text}
                     </div>
